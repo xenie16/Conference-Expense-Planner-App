753994484e5295cf191c9b81a97d515a429a9f6d
Haike <haike16@hotmail.com>
2024-12-12 20:45:06 +0100

    Added capacity when available

--- src/components/SelectionCard.jsx
+++ src/components/SelectionCard.jsx
@@ -10,15 +10,15 @@ function SelectionCard({
             <article className="selection-card" key={option.id}>
                <img className="selection-card-img" src={option.img} alt={option.name}></img>
                <p className="selection-card-title">{option.name}</p>
-               
+               {option.capacity && <p className="selection-card-capacity">Capacity: {option.capacity}</p>}
                <p className="selection-card-price">€ {option.price}</p>
                <div className="selection-card-quantity">
                   <button className="selection-card-button selection-card-button-minus">-</button>
                   <form key={`form-${option.id}`}>
                      <input type="number" key={`input-${option.id}`}></input>
-            </form>
+                  </form>
                   <button className="selection-card-button selection-card-button-plus">+</button>
-         </div>
+               </div>
             </article>
             
          )
