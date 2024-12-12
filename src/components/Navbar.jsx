f9ebedfccbb5ba9d2acc74e864bbc9dd108fc6e4
Haike <haike16@hotmail.com>
2024-12-12 22:38:51 +0100

    h1 Redirects to root

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
-   
+   const navigate = useNavigate();
+   const handleRedirect = () => {
+      navigate('/');
+   }
+
    return (
       <div className="navbar">
-         <h1>Conference Expense Planner</h1>
+         <h1 onClick={handleRedirect}>Conference Expense Planner</h1>
          <div className="navbar-links">
             <a>Venue</a>
             <a>Add-ons</a>
